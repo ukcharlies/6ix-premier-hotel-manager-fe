c123c2f2d8e5cb2bb5682fcc1dcf278fb25ab99e
6ix <Ukcharlies@gmail.com>
2025-12-07 09:39:26 +0100

    feat: add configuration files for project setup and aliasing

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@/components": path.resolve(__dirname, "./src/components"),
+      "@/lib": path.resolve(__dirname, "./src/lib"),
+      "@/utils": path.resolve(__dirname, "./src/utils"),
+      "@/services": path.resolve(__dirname, "./src/services"),
+      "@/contexts": path.resolve(__dirname, "./src/contexts"),
+      "@/pages": path.resolve(__dirname, "./src/pages"),
+      "@/assets": path.resolve(__dirname, "./src/assets"),
+    },
+  },
   server: {
     port: 3000,
   },
